d195cbfab12e96f354861e481f5189ca0593f5d8
Raphael Lima <raphael.aolima8@gmail.com>
2017-08-03 15:49:24 -0300

    Teste de build

--- gulpfile.js
+++ gulpfile.js
@@ -9,7 +9,7 @@ gulp.task("compile", function () {
         .js.pipe(gulp.dest("dist"));
 });
 
-gulp.task("build", ['clean', 'compile'], function() {
+gulp.task("copy-opts", ['clean', 'compile'], function() {
   return gulp.src('tests/unit/config/mocha.opts')
              .pipe(gulp.dest('dist/tests/unit/config'))
              .pipe(gulp.dest('dist/tests/integration/config'))
@@ -18,6 +18,16 @@ gulp.task("build", ['clean', 'compile'], function() {
 gulp.task("clean", function() {
   return gulp.src('dist')
              .pipe(clean());
+});
+
+gulp.task("copy-migration-config", ['clean', 'compile', 'copy-opts'], function() {
+  return gulp.src('server/config/config.json')
+             .pipe(gulp.dest('dist/server/config'))
+});
+
+gulp.task("build", ['clean', 'compile', 'copy-opts', 'copy-migration-config'], function() {
+  return gulp.src('server/migrations/*')
+             .pipe(gulp.dest('dist/server/migrations'))
 });
 
 gulp.task('default', ['build']);

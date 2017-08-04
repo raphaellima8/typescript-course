59fdf50e2188cbeaae016050b60ce2da4d6b7f87
Raphael Lima <raphael.aolima8@gmail.com>
2017-08-03 21:42:34 -0300

    Configura app no Heroku

--- dist/server/config/env/production.env.js
+++ dist/server/config/env/production.env.js
@@ -1,12 +1,5 @@
 module.exports = {
-    env: 'development',
-    db: 'ts-api',
-    dialect: 'postgres',
-    username: 'postgres',
-    password: 'pgroot',
-    host: 'localhost',
-    serverPort: 3000,
-    pgPort: 5432,
-    dbURL: 'postgres://postgres:pgroot@localhost:5432/ts-api',
-    secret: 'S3cr3t'
+    env: process.env.NODE_ENV,
+    dbURL: process.env.DATABASE_URL,
+    secret: process.env.SECRET
 };

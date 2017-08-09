76c4b0e9982ac6946480b9df98f42b12f60e0426
Raphael Lima <raphael.aolima8@gmail.com>
2017-08-08 23:18:40 -0300

    Fix some features

--- dist/server/config/env/production.env.js
+++ dist/server/config/env/production.env.js
@@ -1,6 +1,12 @@
 module.exports = {
-    env: process.env.NODE_ENV,
-    serverPort: 5000,
-    dbURL: process.env.DATABASE_URL,
-    secret: process.env.SECRET
+    env: 'production',
+    db: 'ts-api',
+    dialect: 'postgres',
+    username: 'postgres',
+    password: 'pgroot',
+    host: 'localhost',
+    serverPort: 3000,
+    pgPort: 5432,
+    dbURL: 'postgres://postgres:pgroot@localhost:5432/ts-api',
+    secret: 'S3cr3t'
 };
